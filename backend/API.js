12b26c34f5c70697f5ef8561a23ed6233958637c
AsimakisZaimis <62053618+AsimakisZaimis@users.noreply.github.com>
2021-04-30 15:21:34 +0300

    Update API.js

--- backend/API.js
+++ backend/API.js
@@ -13,11 +13,36 @@ Router
     version: '0.1'
   }, res);
 })
-.add(function(req, res) {
-  response({
-    success: true
-  }, res);
-});
+.add('api/user/login', function(req, res) {
+  processPOSTRequest(req, function(data) {
+    if(!data.email || data.email === '' || !validEmail(data.email)) {
+      error('Invalid or missing email.', res);
+    } else if(!data.password || data.password === '') {
+      error('Please fill your password.', res);
+    } else {
+      getDatabaseConnection(function(db) {
+        var collection = db.collection('users');
+        collection.find({ 
+          email: data.email,
+          password: sha1(data.password)
+        }).toArray(function(err, result) {
+          if(result.length === 0) {
+            error('Wrong email or password', res);
+          } else {
+            var user = result[0];
+            delete user.password;
+            delete user._id;
+            req.session.user = user;
+            response({
+              success: 'OK',
+              user: user
+            }, res);
+          }
+        });
+      });
+    }
+  });
+})
 
 module.exports = function(req, res) {
   Router.check(req.url, [req, res]);
