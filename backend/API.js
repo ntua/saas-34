a40e1bb4a66fff99a587e52e017aea755a7443d1
Alexandros Doukas <doukasfensing@gmail.com>
2021-05-07 15:45:27 +0300

    Update API.js
    Πρόσθεσα τα api/user/logout, api/user

--- backend/API.js
+++ backend/API.js
@@ -7,8 +7,6 @@ var error = function(message, res) {
   res.end(JSON.stringify({error: message}) + '\n');
 }
 
-
-
 var MongoClient = require('mongodb').MongoClient;
 var database;
 var getDatabaseConnection = function(callback) {
@@ -82,3 +80,52 @@ Router
 module.exports = function(req, res) {
   Router.check(req.url, [req, res]);
 }
+
+.add('api/user/logout', function(req, res) {
+  delete req.session.user;
+  response({
+    success: 'OK'
+  }, res);
+})
+.add('api/user', function(req, res) {
+  switch(req.method) {
+    case 'GET':
+      if(req.session && req.session.user) {
+        response(req.session.user, res);
+      } else {
+        response({}, res);
+      }
+    break;
+    case 'PUT':
+      processPOSTRequest(req, function(data) {
+        if(!data.firstName || data.firstName === '') {
+          error('Please fill your first name.', res);
+        } else if(!data.lastName || data.lastName === '') {
+          error('Please fill your last name.', res);
+        } else {
+          getDatabaseConnection(function(db) {
+            var collection = db.collection('users');
+            if(data.password) {
+              data.password = sha1(data.password);
+            }
+            collection.update(
+              { email: req.session.user.email },
+              { $set: data }, 
+              function(err, result) {
+                if(err) {
+                  err('Error updating the data.');
+                } else {
+                  if(data.password) delete data.password;
+                  for(var key in data) {
+                    req.session.user[key] = data[key];
+                  }
+                  response({
+                    success: 'OK'
+                  }, res);
+                }
+              }
+            );
+          });
+        }
+      });
+    break;
