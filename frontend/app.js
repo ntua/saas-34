4916e52b0994e0b98128d2abaaba8e797772b616
Alexandros Doukas <doukasfensing@gmail.com>
2021-05-08 18:18:43 +0300

    Query Selector
    Added query selector

--- frontend/app.js
+++ frontend/app.js
@@ -12,17 +12,40 @@ var showPage = function(newPage) {
 
 window.onload = function() {
 
-  body = document.querySelector('body');
-
-  Router
-  .add('home', function() {
-    var p = new Home();
-    showPage(p);
-  })
-  .add(function() {
-    Router.navigate('home');
-  })
-  .listen()
-  .check();
+  body = document.querySelector('body .container');
+  userModel = new UserModel();
+  userModel.fetch(function(error, result) {
+    Router
+    .add('home', function() {
+      var p = new Home();
+      showPage(p);
+    })
+    .add('register', function() {
+      var p = new Register();
+      showPage(p);
+    })
+    .add('login', function() {
+      var p = new Login();
+      showPage(p);
+    })
+    .add('logout', function() {
+      userModel.logout(function(error, result) {
+        window.location.href = '/';
+      });
+    })
+    .add('profile', function() {
+      if(userModel.isLogged()) {
+        var p = new Profile();
+        showPage(p);
+      } else {
+        Router.navigate('login');
+      }      
+    })
+    .add(function() {
+      Router.navigate('home');
+    })
+    .listen()
+    .check();
+  });
 
 }
